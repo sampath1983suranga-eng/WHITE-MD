aefc811235e992005257445a3e77c097981d9f11
Hashan000-1 <pabasarahashan977@gmail.com>
2025-07-31 22:01:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "mIkBRTyK#4-HH67Q2_CWf6ApKWaWAhlmp2umIMpuRZ9UqoAfrmuQ",
+SESSION_ID: process.env.SESSION_ID || "rMVXyQ7B#ul1eKCFQhHPPTzrBgDPtss1PS9LpYTmkLFpvm6AR44U",
 ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> ",
 };
