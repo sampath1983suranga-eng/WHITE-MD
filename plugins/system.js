312f99ac2013825402d87617b2139429a05f4da7
Hashan000-1 <pabasarahashan977@gmail.com>
2025-08-03 18:36:16 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,24 +1,35 @@
-const config = require('../config')
 const {cmd , commands} = require('../command')
 const os = require("os")
 const {runtime} = require('../lib/functions')
+
 cmd({
     pattern: "system",
-    alias: ["status","botinfo"],
-    desc: "Check uptime , ram usage and more",
+    desc: "Check runtime, owner & more...",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `*Uptime:*  ${runtime(process.uptime())}
-*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*HostName:* ${os.hostname()}
-*Owner:* Hashan Sathsara
-`
-return reply('${status}')
+
+const {readEnv} = require('../lib/database')
+const config = await readEnv();
+if(config.BLOCK_JID.includes(from)) return
+    
+let status = `┌───────────────────────
+├ ⏰ *Runtime:-* ${runtime(process.uptime())}
+├ 📟 *Ram usage:-* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+├ ⚙️ *Platform:-* ${os.hostname()}
+├ 👨‍💻 *Owner:-* HASHAN-MD
+├ 🧬 *Version:-* 1.0
+└───────────────────────
+
+> POWERED BY HASHAN-MD`
+
+return reply(`${status}`)
 
 }catch(e){
 console.log(e)
-reply('${e}')
+reply(`${e}`)
 
+}
+})
